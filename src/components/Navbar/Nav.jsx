b502b45f9909e784906c2f29124777fecadc96f5
Abid Al Wassie <abidalwassie@outlook.com>
2024-08-29 19:33:51 +0600

    minor fixes

--- src/components/Navbar/Nav.jsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import Image from "next/image";
-import { usePathname } from "next/navigation";
-import React, { useState } from "react";
-import { FaSearch } from "react-icons/fa";
-
-export const Nav = () => {
-  return;
-};
