805240bf446e23a95d6e022f882e068e0eb60c75
Abid Al Wassie <abidalwassie@outlook.com>
2024-08-31 14:23:14 +0600

    fix: removed delay from editor contribute page

--- src/app/contribute/page.tsx
+++ src/app/contribute/page.tsx
@@ -1,22 +1,4 @@
-"use client";
-import { Suspense, lazy, useEffect, useState } from "react";
-
-const Editor = lazy(() => import("@/components/ui/Editor"));
-
-export default function Contribute() {
-  const [showEditor, setShowEditor] = useState(false);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowEditor(true);
-    }, 2000); // Adjust the delay as needed (2000 ms = 2 seconds)
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      {showEditor && <Editor />}
-    </Suspense>
-  );
+import { Editor } from "@/components";
+export default async function Contribute() {
+  return <Editor />;
 }
