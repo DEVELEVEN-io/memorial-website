046b8fed3ce38712b44b29dbb671243a33d7e379
Abid Al Wassie <abidalwassie@outlook.com>
2024-09-03 16:41:06 +0600

    fix: minor styles fix for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,23 +25,25 @@ export default function Main() {
 
   return (
     <div className="container mx-auto px-4">
-      <h2 className="mb-4 text-xl">Posts:</h2>
-      <ul className="space-y-4">
-        {posts.map((post) => (
-          <li key={post.id} className="rounded-md border p-4">
-            <h3 className="text-lg font-semibold">{post.title}</h3>
-            <div
-              dangerouslySetInnerHTML={{ __html: post.content }} // Render HTML content
-            />
-            <p className="text-sm text-gray-400">
-              By{" "}
-              <span className="text-blue-400">
-                {post.user?.name || "Unknown"}
-              </span>
-            </p>
-          </li>
-        ))}
-      </ul>
+      <div>
+        <h2 className="mb-4 text-xl">Posts:</h2>
+        <ul className="editor-container space-y-4">
+          {posts.map((post) => (
+            <li key={post.id} className="rounded-md border p-4">
+              <h3 className="text-lg font-semibold">{post.title}</h3>
+              <div
+                dangerouslySetInnerHTML={{ __html: post.content }} // Render HTML content
+              />
+              <p className="text-sm text-gray-400">
+                By{" "}
+                <span className="text-blue-400">
+                  {post.user?.name || "Unknown"}
+                </span>
+              </p>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
