34361ad8ac34194a4a943a9cab80e44815d4d901
Abid Al Wassie <abidalwassie@outlook.com>
2024-09-20 01:29:30 +0600

    fix: updated next config

--- next.config.mjs
+++ next.config.mjs
@@ -15,7 +15,9 @@ const nextConfig = {
           {
             key: "Access-Control-Allow-Origin",
             value:
-              "https://memorial-website-three.vercel.app/api/auth/callback/github/",
+              process.env.NODE_ENV === "production"
+                ? "https://memorial-website-three.vercel.app"
+                : "http://localhost:3000", // Allow localhost in development
           },
           {
             key: "Access-Control-Allow-Methods",
@@ -25,6 +27,10 @@ const nextConfig = {
             key: "Access-Control-Allow-Headers",
             value:
               "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+          {
+            key: "Access-Control-Max-Age",
+            value: "86400", // Cache preflight response for 24 hours
           },
         ],
       },
